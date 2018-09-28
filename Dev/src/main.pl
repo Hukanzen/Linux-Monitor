7ccb0a14ff2f9d472b9f500620dcb91bf651f53f
Hukanzen <qazxswed1934@gmail.com>
2018-09-29 04:39:11 +0900

    dockerコンテナ化

--- Dev/src/main.pl
+++ Dev/src/main.pl
@@ -6,13 +6,33 @@ use utf8;
 
 use Data::Dumper;
 
+
 &main;
+
 
 sub main
 {
+	&initial;
+	
 	my @usage=split(/,/,`Clang/SystemAnalyzer.out`);
 	
 	print Dumper @usage;
 	
+	&clean;
+	
 	return 0;
+}
+
+sub initial
+{
+	chdir('Clang');
+	system('make');
+	chdir('../');
+}
+
+sub clean
+{
+	chdir('Clang');
+	system('make clean');
+	chdir('../');
 }
