bf297b704fdef38706af40e5552efeb7527340b9
Hukanzen <qazxswed1934@gmail.com>
2019-08-31 17:25:58 +0900

    複数の取得

--- Dev/client/src/main.pl
+++ Dev/client/src/main.pl
@@ -7,9 +7,11 @@ use warnings;
 use Net::SNMP;
 
 my @machine_list=('192.168.1.3','192.168.1.3');
+my @old_list=('1.3.6.1.2.1.1.2.0','1.3.6.1.2.1.1.3.0');
 
+my $idx=0;
 foreach my $machine (@machine_list){
-	my $OID_sysUpTime = '1.3.6.1.2.1.1.3.0';
+	my $OID_sysUpTime = $old_list[$idx];
 	
 	my ($session, $error) = Net::SNMP->session(
 		-hostname  => $machine , 
@@ -26,12 +28,13 @@ foreach my $machine (@machine_list){
 	if (!defined $result) {
 		printf "ERROR: %s.\n", $session->error();
 		$session->close();
-		exit 1;
+	#	exit 1;
 	}
 	
 	printf "The sysUpTime for host '%s' is %s.\n",
 		$session->hostname(), $result->{$OID_sysUpTime};
 		$session->close();
-	
+
+	$idx++;
 }
 exit 0;
