4208feeca04f6066351161f74a94a41b73b60d14
Hukanzen <qazxswed1934@gmail.com>
2019-08-31 18:51:25 +0900

    sqlファイルを分割

---
+++ Dev/server/mysql/initdb/m1_user.sql
@@ -0,0 +1,2 @@
+/* ユーザ作成 */
+CREATE USER 'user1'@'10.0.1.%' IDENTIFIED BY 'password';
---
+++ Dev/server/mysql/initdb/m3_list.sql
@@ -0,0 +1,16 @@
+GRANT  INSERT,SELECT,UPDATE, ON `List`.* TO 'user1'@'10.0.1.%';
+
+CREATE DATABASE IF NOT EXISTS `List` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+CREATE TABLE `List`.`Connection`(
+	ID  INTEGER UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY, /* 入力idx */
+	ipaddr    INTEGER UNSIGNED NOT NULL,  /* IPアドレス */
+	name      VARCHAR(30) NOT NULL        /* マシン名 */
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+CREATE TABLE `List`.`MIBNodeOIDNumber`(
+	ID  INTEGER UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY, /* 入力idx */
+	ipaddr    INTEGER UNSIGNED NOT NULL,  /* IPアドレス */
+	oid       VARCHAR(50) NOT NULL        /* マシン名 */
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
