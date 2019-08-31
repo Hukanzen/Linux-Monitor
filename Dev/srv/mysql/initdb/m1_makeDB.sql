eb93c8d65f42b343cdcd4a70709f7833ee0697fe
Hukanzen <qazxswed1934@gmail.com>
2019-08-31 18:09:55 +0900

    接続IP保管テーブルと，MIB保管テーブル

--- Dev/srv/mysql/initdb/m1_makeDB.sql
+++ Dev/srv/mysql/initdb/m1_makeDB.sql
@@ -2,8 +2,10 @@
 CREATE USER 'user1'@'10.0.1.%' IDENTIFIED BY 'password';
 
 CREATE DATABASE IF NOT EXISTS `Machine` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+CREATE DATABASE IF NOT EXISTS `List` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
-GRANT  INSERT,SELECT,UPDATE ON `Machine`.* TO 'user1'@'10.0.1.%';
+GRANT  INSERT,SELECT,UPDATE,CREATE, ON `Machine`.* TO 'user1'@'10.0.1.%';
+GRANT  INSERT,SELECT,UPDATE, ON `List`.* TO 'user1'@'10.0.1.%';
 
 CREATE TABLE `Machine`.`LoadAverage`(
 	ID  INTEGER UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY, /* 入力idx */
@@ -42,3 +44,15 @@ CREATE TABLE `Machine`.`Temperature`(
 	temperature INTEGER        NOT NULL   /* 温度 */
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
+
+CREATE TABLE `List`.`Connection`(
+	ID  INTEGER UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY, /* 入力idx */
+	ipaddr    INTEGER UNSIGNED NOT NULL,  /* IPアドレス */
+	name      VARCHAR(30) NOT NULL        /* マシン名 */
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+CREATE TABLE `List`.`MIBNodeOIDNumber`(
+	ID  INTEGER UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY, /* 入力idx */
+	ipaddr    INTEGER UNSIGNED NOT NULL,  /* IPアドレス */
+	oid       VARCHAR(50) NOT NULL        /* マシン名 */
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
