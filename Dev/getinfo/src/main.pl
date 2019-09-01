81c02ce598e6be35c02f7b83d832b8175a06d69f
Hukanzen <qazxswed1934@gmail.com>
2019-09-01 17:14:54 +0900

    DBDでエラーを解決できず

--- Dev/getinfo/src/main.pl
+++ Dev/getinfo/src/main.pl
@@ -1,11 +1,14 @@
 #!/usr/bin/env perl
 
-
 use strict;
 use warnings;
 use Data::Dumper;
 
 use Net::SNMP;
+
+use lib qw(./lib);   # ./lib配下を読み込む
+use mysqli_connection;
+
 
 my $DB_NAME='List';
 my $DB_ADDR='mysql';
@@ -21,32 +24,32 @@ exit ;
 
 
 
-my $idx=0;
-foreach my $machine (@machine_list){
-	my $OID_sysUpTime = $old_list[$idx];
-	
-	my ($session, $error) = Net::SNMP->session(
-		-hostname  => $machine['addr'] , 
-		-community => 'public',
-	);
-	
-	if (!defined $session) {
-		printf "ERROR: %s.\n", $error;
-		exit 1;
-	}
-	
-	my $result = $session->get_request(-varbindlist => [ $OID_sysUpTime ],);
-	
-	if (!defined $result) {
-		printf "ERROR: %s.\n", $session->error();
-		$session->close();
-	#	exit 1;
-	}
-	
-	printf "The sysUpTime for host '%s' is %s.\n",
-		$session->hostname(), $result->{$OID_sysUpTime};
-		$session->close();
-
-	$idx++;
-}
-exit 0;
+#my $idx=0;
+#foreach my $machine (@machine_list){
+#	my $OID_sysUpTime = $old_list[$idx];
+#	
+#	my ($session, $error) = Net::SNMP->session(
+#		-hostname  => $machine['addr'] , 
+#		-community => 'public',
+#	);
+#	
+#	if (!defined $session) {
+#		printf "ERROR: %s.\n", $error;
+#		exit 1;
+#	}
+#	
+#	my $result = $session->get_request(-varbindlist => [ $OID_sysUpTime ],);
+#	
+#	if (!defined $result) {
+#		printf "ERROR: %s.\n", $session->error();
+#		$session->close();
+#	#	exit 1;
+#	}
+#	
+#	printf "The sysUpTime for host '%s' is %s.\n",
+#		$session->hostname(), $result->{$OID_sysUpTime};
+#		$session->close();
+#
+#	$idx++;
+#}
+#exit 0;
